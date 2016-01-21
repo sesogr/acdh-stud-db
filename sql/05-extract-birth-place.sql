daefd14f876e42ad668fc751331e1dbbed45e592
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2016-01-21 13:17:44 +0100

    Differentiate between the two country values in birth place extraction script

--- sql/05-extract-birth-place.sql
+++ sql/05-extract-birth-place.sql
@@ -5,9 +5,10 @@ CREATE TABLE `student_birth_place_value` AS
 	SELECT DISTINCT
 		`merged_id` AS `person_id`,
 		`geb_ort` AS `birth_place`,
-		`geb_land` AS `birth_country`
+		`geb_land` AS `birth_country_historic`,
+		`geb_land_heute` AS `birth_country_today`
 	FROM `student_person`
-	WHERE `geb_ort` IS NOT NULL OR `geb_land` IS NOT NULL
+	WHERE `geb_ort` IS NOT NULL OR `geb_land` IS NOT NULL OR `geb_land_heute` IS NOT NULL
 	ORDER BY `merged_id`, `birth_place`;
 
 ALTER TABLE `student_birth_place_value`
@@ -28,10 +29,14 @@ CREATE TABLE `student_birth_place_time` AS
 				`year_min`,
 				`year_max`,
 				`geb_ort` AS `birth_place`,
-				`geb_land` AS `birth_country`
+				`geb_land` AS `birth_country_historic`,
+		`geb_land_heute` AS `birth_country_today`
 			FROM `student_person`
 		) AS `s`
-		JOIN `student_birth_place_value` AS `v` ON `v`.`person_id` = `s`.`merged_id` AND `v`.`birth_place` = `s`.`birth_place` AND `v`.`birth_country` = `s`.`birth_country`
+		JOIN `student_birth_place_value` AS `v` ON `v`.`person_id` = `s`.`merged_id`
+			AND `v`.`birth_place` = `s`.`birth_place`
+			AND `v`.`birth_country_historic` = `s`.`birth_country_historic`
+			AND `v`.`birth_country_today` = `s`.`birth_country_today`
 	ORDER BY `s`.`merged_id`, `s`.`birth_place`, `s`.`semester`;
 
 ALTER TABLE `student_birth_place_time`
