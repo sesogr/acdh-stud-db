9519444cc6cd3fc93af8d055c1b188fc8c074d50
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2016-12-06 00:20:24 +0100

    Fill in default value for existing records

--- sql/21-introduce-faculty.sql
+++ sql/21-introduce-faculty.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `student_attendance`
 	ADD COLUMN `faculty` VARCHAR(255) DEFAULT NULL AFTER `class_extra`;
+
+UPDATE `student_attendance`
+SET `faculty` = 'Phil. Fak.'
+WHERE `faculty` IS NULL;
