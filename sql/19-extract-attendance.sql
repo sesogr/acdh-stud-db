8094824690f65c0187aa81d5e50eee7afaec57d6
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2016-12-09 11:04:20 +0100

    Include new records in extraction of student attendance

--- sql/19-extract-attendance.sql
+++ sql/19-extract-attendance.sql
@@ -1,16 +1,36 @@
 DROP TABLE IF EXISTS `student_attendance`;
 
 CREATE TABLE `student_attendance` AS
-	SELECT DISTINCT
-		`p`.`merged_id` `person_id`,
+	SELECT
+		`person_id`,
 		ifnull(`l`.`x_semester`, `p`.`semester`) `semester_abs`,
-		`l`.`x_semester_extra` `semester_rel`,
-		`l`.`x_lecturer` `lecturer`,
-		`l`.`x_class` `class`,
-		`l`.`x_class_extra` `class_extra`,
-		`l`.`anmerkungen` `remarks`
-	FROM `student_lecture` `l`
-		JOIN `student_person` `p` ON p.student_id = l.student_id;
+		`semester_rel`,
+		`lecturer`,
+		`class`,
+		`remarks`
+	FROM (
+			(
+				SELECT DISTINCT
+					`merged_id` AS `person_id`,
+					`x_semester`,
+					`x_semester_extra` `semester_rel`,
+					`x_lecturer` `lecturer`,
+					`x_class` `class`,
+					concat_ws(';', nullif(`x_class_extra`, ''), nullif(`anmerkungen`, '')) `remarks`
+				FROM `student_lecture`
+			)
+			UNION (
+				SELECT DISTINCT
+					`merged` AS `person_id`,
+					`ws_ss`,
+					`semester` `semester_rel`,
+					`dozent` `lecturer`,
+					`vorlesung` `class`,
+					`anmerkung` `remarks`
+				FROM `student_lecture_20161116`
+			)
+		) `l`
+	JOIN `student_person` `p` ON `p`.`student_id` = `l`.`person_id`;
 
 ALTER TABLE `student_attendance`
 ADD COLUMN `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
