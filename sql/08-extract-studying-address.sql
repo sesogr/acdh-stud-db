452154b525944d5c4e29620d4d36e721fb163927
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2016-12-09 11:04:20 +0100

    Include new records in extraction of studying address

--- sql/08-extract-studying-address.sql
+++ sql/08-extract-studying-address.sql
@@ -2,12 +2,21 @@ DROP TABLE IF EXISTS `student_studying_address_time`;
 DROP TABLE IF EXISTS `student_studying_address_value`;
 
 CREATE TABLE `student_studying_address_value` AS
-	SELECT DISTINCT
-		`merged_id` AS `person_id`,
-		`wohnadr_stud` AS `studying_address`
-	FROM `student_person`
-	WHERE `wohnadr_stud` IS NOT NULL
-	ORDER BY `merged_id`, `studying_address`;
+	(
+		SELECT DISTINCT
+			`merged_id` AS `person_id`,
+			`wohnadr_stud` AS `studying_address`
+		FROM `student_person`
+		WHERE `wohnadr_stud` IS NOT NULL
+	)
+	UNION (
+		SELECT DISTINCT
+			`id` AS `person_id`,
+			`adresse_student` AS `studying_address`
+		FROM `student_person_20161116`
+		WHERE `adresse_student` IS NOT NULL
+	)
+	ORDER BY `person_id`, `studying_address`;
 
 ALTER TABLE `student_studying_address_value`
 ADD COLUMN `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
@@ -21,13 +30,24 @@ CREATE TABLE `student_studying_address_time` AS
 		`year_max`
 	FROM
 		(
-			SELECT DISTINCT
-				`merged_id`,
-				`semester`,
-				`year_min`,
-				`year_max`,
-				`wohnadr_stud` AS `studying_address`
-			FROM `student_person`
+			(
+				SELECT DISTINCT
+					`merged_id`,
+					`semester`,
+					`year_min`,
+					`year_max`,
+					`wohnadr_stud` AS `studying_address`
+				FROM `student_person`
+			)
+			UNION (
+				SELECT DISTINCT
+					`id` AS `merged_id`,
+					`semester`,
+					`year_min`,
+					`year_max`,
+					`adresse_student` AS `studying_address`
+				FROM `student_person_20161116`
+			)
 		) AS `s`
 		JOIN `student_studying_address_value` AS `v` ON `v`.`person_id` = `s`.`merged_id` AND `v`.`studying_address` = `s`.`studying_address`
 	ORDER BY `s`.`merged_id`, `s`.`studying_address`, `s`.`semester`;
