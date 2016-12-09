b07f93507a927123879df8df11521bfc0f03d9e3
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2016-12-09 11:04:20 +0100

    Include new records in extraction of graduation

--- sql/14-extract-graduation.sql
+++ sql/14-extract-graduation.sql
@@ -2,12 +2,21 @@ DROP TABLE IF EXISTS `student_graduation_time`;
 DROP TABLE IF EXISTS `student_graduation_value`;
 
 CREATE TABLE `student_graduation_value` AS
-	SELECT DISTINCT
-		`merged_id` AS `person_id`,
-		`prom` AS `graduation`
-	FROM `student_person`
-	WHERE `prom` IS NOT NULL
-	ORDER BY `merged_id`, `graduation`;
+	(
+		SELECT DISTINCT
+			`merged_id` AS `person_id`,
+			`prom` AS `graduation`
+		FROM `student_person`
+		WHERE `prom` IS NOT NULL
+	)
+	UNION (
+		SELECT DISTINCT
+			`id` AS `person_id`,
+			`hinweise_zur_promotion` AS `graduation`
+		FROM `student_person_20161116`
+		WHERE `hinweise_zur_promotion` IS NOT NULL
+	)
+	ORDER BY `person_id`, `graduation`;
 
 ALTER TABLE `student_graduation_value`
 ADD COLUMN `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
@@ -21,13 +30,24 @@ CREATE TABLE `student_graduation_time` AS
 		`year_max`
 	FROM
 		(
-			SELECT DISTINCT
-				`merged_id`,
-				`semester`,
-				`year_min`,
-				`year_max`,
-				`prom` AS `graduation`
-			FROM `student_person`
+			(
+				SELECT DISTINCT
+					`merged_id`,
+					`semester`,
+					`year_min`,
+					`year_max`,
+					`prom` AS `graduation`
+				FROM `student_person`
+			)
+			UNION (
+				SELECT DISTINCT
+					`id` AS `merged_id`,
+					`semester`,
+					`year_min`,
+					`year_max`,
+					`hinweise_zur_promotion` AS `graduation`
+				FROM `student_person_20161116`
+			)
 		) AS `s`
 		JOIN `student_graduation_value` AS `v` ON `v`.`person_id` = `s`.`merged_id` AND `v`.`graduation` = `s`.`graduation`
 	ORDER BY `s`.`merged_id`, `s`.`graduation`, `s`.`semester`;
