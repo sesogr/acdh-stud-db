52b1549bd15908ca1b4b89fbc7d93cf30a9a5c3f
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2016-12-09 11:04:20 +0100

    Include new records in extraction of remarks

--- sql/16-extract-remarks.sql
+++ sql/16-extract-remarks.sql
@@ -2,12 +2,21 @@ DROP TABLE IF EXISTS `student_remarks_time`;
 DROP TABLE IF EXISTS `student_remarks_value`;
 
 CREATE TABLE `student_remarks_value` AS
-	SELECT DISTINCT
-		`merged_id` AS `person_id`,
-		`anmerkung` AS `remarks`
-	FROM `student_person`
-	WHERE `anmerkung` IS NOT NULL
-	ORDER BY `merged_id`, `remarks`;
+	(
+		SELECT DISTINCT
+			`merged_id` AS `person_id`,
+			`anmerkung` AS `remarks`
+		FROM `student_person`
+		WHERE `anmerkung` IS NOT NULL
+	)
+	UNION (
+		SELECT DISTINCT
+			`id` AS `person_id`,
+			`anmerkung` AS `remarks`
+		FROM `student_person_20161116`
+		WHERE `anmerkung` IS NOT NULL
+	)
+	ORDER BY `person_id`, `remarks`;
 
 ALTER TABLE `student_remarks_value`
 ADD COLUMN `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
@@ -21,13 +30,24 @@ CREATE TABLE `student_remarks_time` AS
 		`year_max`
 	FROM
 		(
-			SELECT DISTINCT
-				`merged_id`,
-				`semester`,
-				`year_min`,
-				`year_max`,
-				`anmerkung` AS `remarks`
-			FROM `student_person`
+			(
+				SELECT DISTINCT
+					`merged_id`,
+					`semester`,
+					`year_min`,
+					`year_max`,
+					`anmerkung` AS `remarks`
+				FROM `student_person`
+			)
+			UNION (
+				SELECT DISTINCT
+					`merged_id`,
+					`semester`,
+					`year_min`,
+					`year_max`,
+					`anmerkung` AS `remarks`
+				FROM `student_person`
+			)
 		) AS `s`
 		JOIN `student_remarks_value` AS `v` ON `v`.`person_id` = `s`.`merged_id` AND `v`.`remarks` = `s`.`remarks`
 	ORDER BY `s`.`merged_id`, `s`.`remarks`, `s`.`semester`;
