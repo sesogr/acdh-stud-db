dcbcb55a20fac76bdfc345067d6618d1992943de
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2017-01-02 15:30:47 +0100

    Move creation of additional US-ASCII columns into own script

---
+++ sql/21-prepare-unicode-decomposition.sql
@@ -0,0 +1,17 @@
+ALTER TABLE `student_last_name_value`
+	ADD `ascii_last_name` VARCHAR(255) DEFAULT NULL;
+
+ALTER TABLE `student_given_names_value`
+	ADD `ascii_given_names` VARCHAR(255) DEFAULT NULL;
+
+ALTER TABLE `student_attendance`
+	ADD `ascii_lecturer` VARCHAR(255) DEFAULT NULL;
+
+UPDATE `student_last_name_value`
+	SET `ascii_last_name` = lower(`last_name`);
+
+UPDATE `student_given_names_value`
+	SET `ascii_given_names` = lower(`given_names`);
+
+UPDATE `student_attendance`
+	SET `ascii_lecturer` = lower(`lecturer`);
