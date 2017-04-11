8b79808455d79d387b013335704462ed62933616
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2017-04-11 14:42:22 +0200

    Add file to merge separated Tauber records; CSV unchanged

---
+++ sql/24-fix-tauber-problem.sql
@@ -0,0 +1,7 @@
+UPDATE `student_lecture`
+SET `merged_id` = 381
+WHERE `merged_id` IN (1477, 2027, 2266);
+
+UPDATE `student_attendance`
+SET `person_id` = 381
+WHERE `person_id` IN (1477, 2027, 2266);
