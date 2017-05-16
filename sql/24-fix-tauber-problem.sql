999deb7ca9812889c731dafb6340f0cf231d6a5d
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2017-05-16 22:00:19 +0200

    Merge 1801 into 381; oeaw-138

--- sql/24-fix-tauber-problem.sql
+++ sql/24-fix-tauber-problem.sql
@@ -1,7 +1,7 @@
 UPDATE `student_lecture`
 SET `merged_id` = 381
-WHERE `merged_id` IN (1477, 2027, 2266);
+WHERE `merged_id` IN (1477, 2027, 2266, 1801);
 
 UPDATE `student_attendance`
 SET `person_id` = 381
-WHERE `person_id` IN (1477, 2027, 2266);
+WHERE `person_id` IN (1477, 2027, 2266, 1801);
