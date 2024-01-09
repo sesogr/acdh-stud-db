0412a64a1a64b2173a6c6f219bfaab6cbf7edef5
sesogr <github-sesogr@sedna-soft.de>
2024-01-09 09:50:20 +0000

    Add SQL to create the is_doubtful columns

---
+++ sql/2023/01-doubtful-columns.sql
@@ -0,0 +1,9 @@
+alter table student_birth_date_value add is_doubtful set ('true') default '' not null;
+alter table student_birth_place_value add is_doubtful set ('true') default '' not null;
+alter table student_father_value add is_doubtful set ('true') default '' not null;
+alter table student_gender_value add is_doubtful set ('true') default '' not null;
+alter table student_given_names_value add is_doubtful set ('true') default '' not null;
+alter table student_guardian_value add is_doubtful set ('true') default '' not null;
+alter table student_last_name_value add is_doubtful set ('true') default '' not null;
+alter table student_last_school_value add is_doubtful set ('true') default '' not null;
+alter table student_remarks_value add is_doubtful set ('true') default '' not null;
