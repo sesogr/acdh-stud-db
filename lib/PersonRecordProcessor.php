7b577e6c56e6e25e98814932d0d673b4e6ce0277
Anja Schaffhirt <anja@sedna-soft.de>
2024-05-17 17:52:06 +0000

    Add doubtful support to PersonRecordProcessor

--- lib/PersonRecordProcessor.php
+++ lib/PersonRecordProcessor.php
@@ -49,20 +49,20 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             'insert into student_identity (person_id, year_min, year_max) values (?, ?, ?)'
         );
         $this->biographyExtractor = new PersonalTraitExtractor($this->pdo, 'biography', ['biography', 'is_from_supplemental_data_source']);
-        $this->birthDateExtractor = new PersonalTraitExtractor($this->pdo, 'birth_date', ['birth_date', 'born_on_or_after', 'born_on_or_before', 'is_from_supplemental_data_source']);
-        $this->birthPlaceExtractor = new PersonalTraitExtractor($this->pdo, 'birth_place', ['birth_place', 'birth_country_historic', 'birth_country_today']);
+        $this->birthDateExtractor = new PersonalTraitExtractor($this->pdo, 'birth_date', ['birth_date', 'born_on_or_after', 'born_on_or_before', 'is_from_supplemental_data_source', 'is_doubtful']);
+        $this->birthPlaceExtractor = new PersonalTraitExtractor($this->pdo, 'birth_place', ['birth_place', 'birth_country_historic', 'birth_country_today', 'is_doubtful']);
         $this->ethnicityExtractor = new PersonalTraitExtractor($this->pdo, 'ethnicity', ['ethnicity']);
-        $this->fatherExtractor = new PersonalTraitExtractor($this->pdo, 'father', ['father']);
-        $this->genderExtractor = new PersonalTraitExtractor($this->pdo, 'gender', ['gender']);
-        $this->givenNamesExtractor = new PersonalTraitExtractor($this->pdo, 'given_names', ['given_names', 'ascii_given_names']);
-        $this->guardianExtractor = new PersonalTraitExtractor($this->pdo, 'guardian', ['guardian']);
+        $this->fatherExtractor = new PersonalTraitExtractor($this->pdo, 'father', ['father', 'is_doubtful']);
+        $this->genderExtractor = new PersonalTraitExtractor($this->pdo, 'gender', ['gender', 'is_doubtful']);
+        $this->givenNamesExtractor = new PersonalTraitExtractor($this->pdo, 'given_names', ['given_names', 'ascii_given_names', 'is_doubtful']);
+        $this->guardianExtractor = new PersonalTraitExtractor($this->pdo, 'guardian', ['guardian', 'is_doubtful']);
         $this->languageExtractor = new PersonalTraitExtractor($this->pdo, 'language', ['language']);
-        $this->lastNameExtractor = new PersonalTraitExtractor($this->pdo, 'last_name', ['last_name', 'ascii_last_name']);
-        $this->lastSchoolExtractor = new PersonalTraitExtractor($this->pdo, 'last_school', ['last_school']);
+        $this->lastNameExtractor = new PersonalTraitExtractor($this->pdo, 'last_name', ['last_name', 'ascii_last_name', 'is_doubtful']);
+        $this->lastSchoolExtractor = new PersonalTraitExtractor($this->pdo, 'last_school', ['last_school', 'is_doubtful']);
         $this->literatureExtractor = new PersonalTraitExtractor($this->pdo, 'literature', ['literature']);
         $this->nationalityExtractor = new PersonalTraitExtractor($this->pdo, 'nationality', ['nationality']);
         $this->religionExtractor = new PersonalTraitExtractor($this->pdo, 'religion', ['religion']);
-        $this->remarksExtractor = new PersonalTraitExtractor($this->pdo, 'remarks', ['remarks']);
+        $this->remarksExtractor = new PersonalTraitExtractor($this->pdo, 'remarks', ['remarks', 'is_doubtful']);
         $this->studyingAddressExtractor = new PersonalTraitExtractor($this->pdo, 'studying_address', ['studying_address']);
     }
 
@@ -99,7 +99,15 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
                                   $isCommentFromSupplementalSources = null,
                                   $literaturhinweise = null,
                                   $anmerkung = null,
-                                  $volkszugehoerigkeit = null): void
+                                  $volkszugehoerigkeit = null,
+                                  $doubtfulBirthDate = false,
+                                  $doubtfulBirthPlace = false,
+                                  $doubtfulFather = false,
+                                  $doubtfulGender = false,
+                                  $doubtfulGuardian = false,
+                                  $doubtfulLastSchool = false,
+                                  $doubtfulName = false,
+                                  $doubtfulRemarks = false): void
     {
         $this->guardPersonRecordIsNew($id, $semester);
         $this->facultyMap[$id] = $fakultaet;
@@ -107,7 +115,7 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $lastName = $matches[1] . ' ' . $matches[2];
             $givenNames = $matches[3];
         } else {
-            [$lastName, $givenNames] = explode(' ', $name, 2);
+            [$lastName, $givenNames] = preg_split('/ |,/', $name, 2);
         }
         if (!$isExistingId) {
             $this->insertIdentityStatement->execute([$id, $yearMin, $yearMax]);
@@ -128,7 +136,8 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $gebDat,
             $bornMin,
             $bornMax,
-            $isDobFromSupplementalSources
+            $isDobFromSupplementalSources,
+            $doubtfulBirthDate
         );
         $this->birthPlaceExtractor->extract(
             $id,
@@ -137,7 +146,8 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $yearMax,
             $geburtsort,
             $geburtslandHistorisch,
-            $geburtslandHeute
+            $geburtslandHeute,
+            $doubtfulBirthPlace
         );
         $this->ethnicityExtractor->extract(
             $id,
@@ -151,14 +161,16 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $semester,
             $yearMin,
             $yearMax,
-            $nameStandUWohnortDVaters
+            $nameStandUWohnortDVaters,
+            $doubtfulFather
         );
         $this->genderExtractor->extract(
             $id,
             $semester,
             $yearMin,
             $yearMax,
-            $geschlecht
+            $geschlecht,
+            $doubtfulGender
         );
         $this->givenNamesExtractor->extract(
             $id,
@@ -166,14 +178,16 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $yearMin,
             $yearMax,
             $givenNames,
-            $this->decomposeUnicode($givenNames)
+            $this->decomposeUnicode($givenNames),
+            $doubtfulName
         );
         $this->guardianExtractor->extract(
             $id,
             $semester,
             $yearMin,
             $yearMax,
-            $vormund
+            $vormund,
+            $doubtfulGuardian
         );
         $this->languageExtractor->extract(
             $id,
@@ -188,14 +202,16 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $yearMin,
             $yearMax,
             $lastName,
-            $this->decomposeUnicode($lastName)
+            $this->decomposeUnicode($lastName),
+            $doubtfulName
         );
         $this->lastSchoolExtractor->extract(
             $id,
             $semester,
             $yearMin,
             $yearMax,
-            $zuletztBesuchteLehranstaltGrundlageFuerImmatriculation
+            $zuletztBesuchteLehranstaltGrundlageFuerImmatriculation,
+            $doubtfulLastSchool
         );
         $this->literatureExtractor->extract(
             $id,
@@ -223,7 +239,8 @@ class PersonRecordProcessor extends RecordProcessor implements FacultyMap
             $semester,
             $yearMin,
             $yearMax,
-            $anmerkung
+            $anmerkung,
+            $doubtfulRemarks
         );
         $this->studyingAddressExtractor->extract(
             $id,
