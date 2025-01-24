df110b1b1520054a77d88fe1c7ca808fb15e2257
Anja Schaffhirt <anja@sedna-soft.de>
2025-01-24 13:44:44 +0000

    Modify detail view to include similarity info (WIP)

--- web/src/show.php
+++ web/src/show.php
@@ -6,10 +6,31 @@
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ));
     $pdo->exec('SET NAMES utf8');
+    $sumPropertyWeights = $pdo->query('select sum(weight) from student_similarity_weight', PDO::FETCH_COLUMN, 0)->fetch();
     $listProperties = $pdo->prepare('SELECT * FROM `v_student_complete` WHERE `person_id` = ?');
     $listLectures = $pdo->prepare('SELECT * FROM `student_attendance` WHERE `person_id` = ? ORDER BY substr(`semester_abs` FROM 4), `lecturer`');
+    $listSimilarStudents = $pdo->prepare(<<<'EOD'
+        select
+            if(id_low = ?, id_high, id_low) other_id,
+            sum(sg.mean * sw.weight) / ? weighted_mean,
+            avg(median) median,
+            min(min) min,
+            max(max) max,
+            count(sg.property) count
+        from `student_similarity_graph` sg
+            join student_similarity_weight sw using (property)
+        where (`id_low` = ? or `id_high` = ?)
+            and `max` > .5
+            and sg.`mean` > .3
+            and `sg`.`property` <> 'birth_date'
+        group by other_id
+        having `weighted_mean` > .3
+        order by weighted_mean desc
+    EOD
+    );
     $listProperties->execute(array($_GET['id']));
     $listLectures->execute(array($_GET['id']));
+    $listSimilarStudents->execute(array($_GET['id'], $sumPropertyWeights, $_GET['id'], $_GET['id']));
     $student = array();
     $hasTimes = false;
     foreach ($listProperties as $property) {
@@ -79,6 +100,30 @@
                     <?php endif ?>
                 </tr>
             <?php endif ?>
+        <?php endforeach ?>
+    </tbody>
+</table>
+<table>
+    <thead>
+        <tr>
+            <th>other_id</th>
+            <th>mean</th>
+            <th>median</th>
+            <th>min</th>
+            <th>max</th>
+            <th>count</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($listSimilarStudents as $similarity): ?>
+            <tr>
+                <td><?php out($similarity['other_id']) ?></td>
+                <td><?php out($similarity['weighted_mean']) ?></td>
+                <td><?php out($similarity['median']) ?></td>
+                <td><?php out($similarity['min']) ?></td>
+                <td><?php out($similarity['max']) ?></td>
+                <td><?php out($similarity['count']) ?></td>
+            </tr>
         <?php endforeach ?>
     </tbody>
 </table>
