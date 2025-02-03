1ac53c49debd7455c9a1dfd689f8d4273927a216
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-03 13:27:17 +0100

    eventlistener outside showhidetoggle

--- web/src/showhidetoggle.js
+++ web/src/showhidetoggle.js
@@ -3,14 +3,16 @@ const showhidetoggle = (checkbox) => {
     .querySelectorAll("tr." + checkbox.dataset.dupeId)
     .forEach((el) => el.classList.toggle("hide"));
 
-document.addEventListener("DOMContentLoaded", () =>
-  document
-    .querySelectorAll('input[type="checkbox"][data-dupe-id]')
-    .forEach((cb) => !cb.checked && showhidetoggle(cb)),
-);
-document.querySelector('input[type="checkbox"]').indeterminate = true;
+    document.querySelector('input[type="checkbox"]').indeterminate = true;
+    
 }
 
+document.addEventListener("DOMContentLoaded", () =>
+    document
+      .querySelectorAll('input[type="checkbox"][data-dupe-id]')
+      .forEach((cb) => !cb.checked && showhidetoggle(cb)),
+  );
+  
 
 const changeall = (checkbox) => {
   let allstates = []
@@ -22,17 +24,16 @@ const changeall = (checkbox) => {
   if (checkbox.checked) {
       for (let i = 0; i<allstates.length; i++){
           if (!allstates[i]){
-              showhidetoggle(allinputs[i])
-              allinputs[i].checked = true
+              allinputs[i].click();
           }
       }
   }else{
       for (let i = 0; i<allstates.length; i++){
           if (allstates[i]){
-              showhidetoggle(allinputs[i])
-              allinputs[i].checked = false
+              allinputs[i].click();
           }
       }
   };
+
   checkbox.indeterminate = false;
 };
