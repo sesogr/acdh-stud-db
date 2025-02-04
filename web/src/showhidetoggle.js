104a7db0e8ca41f79c1c4bc66f88cdfea099836a
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-04 10:21:07 +0100

    refactor showhidetoggle logic for checkbox indeterminate state

--- web/src/showhidetoggle.js
+++ web/src/showhidetoggle.js
@@ -3,18 +3,22 @@ const showhidetoggle = (checkbox) => {
     .querySelectorAll("tr." + checkbox.dataset.dupeId)
     .forEach((el) => el.classList.toggle("hide"));
     
-    const allcheckbox = document.querySelectorAll('input[type="checkbox"][data-dupe-id]')
-    let indetermin = false
-    allcheckbox.forEach((e) => {
-        if (e.checked != checkbox.checked) {
-            indetermin = true;
-        }
-    });
-    document.querySelector('input[type="checkbox"]').indeterminate = false;
-    if(indetermin){
-        document.querySelector('input[type="checkbox"]').indeterminate = true;
+    const allcheckboxes = document.querySelectorAll('input[type="checkbox"][data-dupe-id]');
+
+    const allcheckbox = document.querySelector('input[type="checkbox"]');
+    
+    if( [...allcheckboxes].filter((e) => e.checked != checkbox.checked).length > 0 ){
+        
+        allcheckbox.indeterminate = true
+        
+    }else{
+
+        allcheckbox.indeterminate = false
+        allcheckbox.checked = checkbox.checked
     }
-}
+    
+};
+
 
 document.addEventListener("DOMContentLoaded", () =>
     document
@@ -27,9 +31,8 @@ const changeall = (checkbox) => {
   let allstates = []
   
   const allinputs = document.querySelectorAll('input[type="checkbox"][data-dupe-id]');
-  allinputs.forEach((e) => {
-      allstates.push(e.checked)
-  });
+  allstates = [...allinputs].map((e) => (e.checked));
+
   if (checkbox.checked) {
       for (let i = 0; i<allstates.length; i++){
           if (!allstates[i]){
@@ -43,6 +46,4 @@ const changeall = (checkbox) => {
           }
       }
   };
-
-  checkbox.indeterminate = false;
 };
