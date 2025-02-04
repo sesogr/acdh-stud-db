c0998a762f4b9056e9d3b960ca03655bc5caa420
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-04 11:10:32 +0100

    rename parameter 'ini' to 'origin' in showhidetoggle function for clarity

--- web/src/showhidetoggle.js
+++ web/src/showhidetoggle.js
@@ -1,4 +1,4 @@
-const showhidetoggle = (checkbox, ini=false) => {
+const showhidetoggle = (checkbox, origin=false) => {
   document
     .querySelectorAll("tr." + checkbox.dataset.dupeId)
     .forEach((el) => el.classList.toggle("hide"));
@@ -6,7 +6,7 @@ const showhidetoggle = (checkbox, ini=false) => {
     const allcheckboxes = document.querySelectorAll('input[type="checkbox"][data-dupe-id]');
 
     const allcheckbox = document.querySelector('input[type="checkbox"]');
-    if(ini) return null;
+    if(origin) return null;
     
     if( [...allcheckboxes].filter((e) => e.checked != checkbox.checked).length > 0 ){
         
