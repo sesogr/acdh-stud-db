1e029f6d1b31c48e1232c1c9e44de300708178b1
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-04 10:33:05 +0100

    skippen der indeterminate logic wenn es von changeall kommt.

--- web/src/showhidetoggle.js
+++ web/src/showhidetoggle.js
@@ -1,4 +1,4 @@
-const showhidetoggle = (checkbox) => {
+const showhidetoggle = (checkbox, ini=false) => {
   document
     .querySelectorAll("tr." + checkbox.dataset.dupeId)
     .forEach((el) => el.classList.toggle("hide"));
@@ -6,17 +6,18 @@ const showhidetoggle = (checkbox) => {
     const allcheckboxes = document.querySelectorAll('input[type="checkbox"][data-dupe-id]');
 
     const allcheckbox = document.querySelector('input[type="checkbox"]');
-    
+    if(ini){
+        return null;
+    };
     if( [...allcheckboxes].filter((e) => e.checked != checkbox.checked).length > 0 ){
         
         allcheckbox.indeterminate = true
-        
+
     }else{
 
         allcheckbox.indeterminate = false
         allcheckbox.checked = checkbox.checked
     }
-    
 };
 
 
@@ -36,14 +37,16 @@ const changeall = (checkbox) => {
   if (checkbox.checked) {
       for (let i = 0; i<allstates.length; i++){
           if (!allstates[i]){
-              allinputs[i].click();
+              showhidetoggle(allinputs[i],true);
+              allinputs[i].checked = checkbox.checked;
           }
       }
   }else{
       for (let i = 0; i<allstates.length; i++){
           if (allstates[i]){
-              allinputs[i].click();
-          }
+              showhidetoggle(allinputs[i],true);
+              allinputs[i].checked = checkbox.checked;
+        }
       }
   };
 };
