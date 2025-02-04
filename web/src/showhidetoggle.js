e94b2582f3284a3f88c5c72920d482d8b7ca1424
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-04 11:01:20 +0100

    changed it into filter

--- web/src/showhidetoggle.js
+++ web/src/showhidetoggle.js
@@ -29,24 +29,12 @@ document.addEventListener("DOMContentLoaded", () =>
   
 
 const changeall = (checkbox) => {
-  let allstates = []
-  
-  const allinputs = document.querySelectorAll('input[type="checkbox"][data-dupe-id]');
-  allstates = [...allinputs].map((e) => (e.checked));
 
-  if (checkbox.checked) {
-      for (let i = 0; i<allstates.length; i++){
-          if (!allstates[i]){
-              showhidetoggle(allinputs[i],true);
-              allinputs[i].checked = checkbox.checked;
-          }
-      }
-  }else{
-      for (let i = 0; i<allstates.length; i++){
-          if (allstates[i]){
-              showhidetoggle(allinputs[i],true);
-              allinputs[i].checked = checkbox.checked;
-        }
-      }
-  };
+  const allcheckboxes = document.querySelectorAll('input[type="checkbox"][data-dupe-id]');
+
+  [...allcheckboxes].filter((e) => e.checked != checkbox.checked).forEach((e) => {
+    e.checked = checkbox.checked
+    showhidetoggle(e, true)}
+  );
+
 };
