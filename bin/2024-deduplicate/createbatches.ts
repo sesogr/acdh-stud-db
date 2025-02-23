ff3a2824d8266f108386aed92df96244f9483d6f
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-23 17:08:21 +0100

    probable fix

--- bin/2024-deduplicate/createbatches.ts
+++ bin/2024-deduplicate/createbatches.ts
@@ -57,6 +57,6 @@ const getnextavailableIds: GetNextAvailableIds = async () => {
     returntype[1][0] || 0,
     returntype[1][1] || 0,
   ];
-  return [low, high, end];
+  return [end, low, high];
 };
 
