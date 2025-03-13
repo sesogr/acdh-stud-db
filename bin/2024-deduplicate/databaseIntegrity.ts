5814f4abb8f8ce18400a62728653dea813406098
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-03-13 14:47:32 +0100

    id_lows without problems

--- bin/2024-deduplicate/databaseIntegrity.ts
+++ bin/2024-deduplicate/databaseIntegrity.ts
@@ -1,4 +1,4 @@
-import { getAllIds, getIdLow } from "./database";
+import { getAllIds, getAllIdLow } from "./database";
 import { createConnection } from "mariadb";
 
 const credentials = {
@@ -10,17 +10,37 @@ const credentials = {
   password: "nJkyj2pOsfUi",
 };
 
-// let allids = new Set(getAllids().map((e) => e[0]));
-// let actallids = new Set(actualids.map((e) => e[0]));
-// let diff = [...allids].filter((x) => !actallids.has(x));
-// console.log(diff);
-
 createConnection(credentials).then((connection) => {
-  Promise.all([
-    getAllIds(connection),
-    getIdLow(connection, "student_similarity_graph"),
-  ]).then((results) => {
-    let allids = results[0];
-    console.log(allids);
+  getAllIds(connection).then((allIdObjects) => {
+    const allIds: number[] = allIdObjects.map(
+      (element: { person_id: number }) => element.person_id
+    );
+    getAllIdLow(connection, "student_similarity_graph").then((idLowObjects) => {
+      connection.end();
+      const idLows: number[] = idLowObjects.map(
+        (element: { id_low: number }) => element.id_low
+      );
+      const missingIds = allIds.filter((individualId) =>
+        idLows.includes(individualId)
+      );
+      console.log(missingIds);
+    });
   });
 });
+
+// createConnection(credentials).then((connection) => {
+// getAllIds(connection).then((allIdObjects) => {
+// const allIds: number[] = allIdObjects.map(
+// (element: { person_id: number }) => element.person_id
+// );
+// const allidPairs: number[][] = allIds.map((e, i, array) => {
+// return [e, ...array.slice(i)];
+// });
+// getAllIdLowHighPairs(
+// connection,
+// "student_similarity_graph_birthrange"
+// ).then((idPairObjects) => {
+// console.log(idPairObjects);
+// });
+// });
+// });
