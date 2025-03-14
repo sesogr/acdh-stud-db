3d6ba0043962bfc4b3bc92659a0668b54e369ed8
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-03-14 13:36:09 +0100

    getIdhighfromlowid specifically

--- bin/2024-deduplicate/database.ts
+++ bin/2024-deduplicate/database.ts
@@ -177,8 +177,11 @@ export const getAllIdLow = (connection: Connection, table: string) => {
     `select distinct id_low from ${table} force index (id_low) order by id_low`
   );
 };
-// TODO: needs 2+GB ram
-export const getAllIdLowHighPairs = (connection: Connection, table: string) => {
+export const getIdHighFromIdLow = (
+  connection: Connection,
+  table: string,
+  idLow: number
+) => {
   return connection.query(`
-    select distinct id_low, id_high from ${table} force index (id_low_2) order by id_low,id_high `);
+    select distinct id_high from ${table} force index (id_low_2) where id_low = ${idLow} order by id_high `);
 };
