4f27fcf3bdb6d8e6909f4425f33c903231a9ce50
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-03-18 10:03:12 +0100

    workerLimit is always at max specified worker count + it now removes the ids from ids.json that got to workers already

--- bin/2024-deduplicate/main.ts
+++ bin/2024-deduplicate/main.ts
@@ -17,12 +17,18 @@ function createworker(workerpath: string = "./worker.js") {
   let idfile = fs.readFileSync(path, "utf-8");
   const id: number[][] = JSON.parse(idfile);
   const workers: any[] = [];
-  for (let i = 1; i < id.length; i++) {
+  const workerLimit = Math.min(id.length, workercount + 1);
+  for (let i = 1; i < workerLimit; i++) {
     const ids = id[i];
     workers.push(
       promiseWorker<ComparisonWorkers>(ids, credentials, workerpath)
     );
   }
+  fs.writeFileSync(
+    "ids.json",
+    JSON.stringify([id[0], ...id.slice(workerLimit)], null, 2),
+    { flag: "w" }
+  );
   Promise.allSettled(workers).then((workerResults) => {
     workerResults.forEach((e) => {
       if (e.status === "rejected") {
@@ -52,7 +58,7 @@ if (fs.existsSync(path) && start != 3) {
 
 // resize workercount to the number of available cores
 // resize batchsize to the a number that each worker takes a reasonable amount of time
-const workercount: number = 1;
+const workercount: number = 12;
 const batchsize: number = 1024;
 
 // comment block for the main graph
@@ -81,4 +87,4 @@ if (start == 2)
 // run this if you want to setup specific ids in ids.json remember to disable the remove ids.json
 // the number of created workers will be the number of arrays in the ids.json file -1 because
 // zero index is an array with the highest possible id in the database
-if (start == 3) createworker("./workerDateRange.js");
+if (start == 3) createworker();
